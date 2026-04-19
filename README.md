fe9e373291e216080dca094b576bbe0db9767ec0
xourhuhb <sourabhalabanoor@gmail.com>
2026-04-19 22:14:26 +0530

    Move API key to environment variables

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Streamix
+
 a YouTube-inspired frontend app using the YouTube API to search, browse, and watch videos with a clean, responsive UI.
--- src/utlis/constants.js
+++ src/utlis/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = "AIzaSyCLPQCL5oYyveyZpp0eRSMYtEDRpbk0Dh0";
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export const YOUTUBE_VIDEO_URL = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`;
 
